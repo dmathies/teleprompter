bfb9018fd292b97dc7977c37013644b87c707473
AdamM <adam@mathieson.dev>
2026-09-03 21:17:35 +0200

    + vite-ify

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+  plugins: [
+    Icons({
+      compiler: 'raw',
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        teleprompter: resolve(import.meta.dirname, 'teleprompter.html'),
+        pen_pointer_diagnostics: resolve(import.meta.dirname, 'pen_pointer_diagnostics.html'),
+        sse_test: resolve(import.meta.dirname, 'sse_test.html'),
+      },
+    },
+  },
+});
